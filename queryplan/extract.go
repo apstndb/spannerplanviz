cd91ee3433cc95c85d8c1f88713211be13d99c95
apstndb <gamaguchi@gmail.com>
2021-03-18 16:39:20 +0900

    Fix format

--- queryplan/extract.go
+++ queryplan/extract.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/genproto/googleapis/spanner/v1"
 	"gopkg.in/yaml.v3"
 )
+
 func ExtractQueryPlan(b []byte) (*spanner.ResultSetStats, *spanner.StructType, error) {
 	var jsonObj map[string]interface{}
 	err := yaml.Unmarshal(b, &jsonObj)
@@ -18,21 +19,21 @@ func ExtractQueryPlan(b []byte) (*spanner.ResultSetStats, *spanner.StructType, e
 		var rss spanner.ResultSetStats
 		err = protoyaml.Unmarshal(b, &rss)
 		if err != nil {
-			return nil,nil,  err
+			return nil, nil, err
 		}
 		return &rss, nil, nil
 	} else if _, ok := jsonObj["planNodes"]; ok {
 		var qp spanner.QueryPlan
 		err = protoyaml.Unmarshal(b, &qp)
 		if err != nil {
-			return nil, nil,  err
+			return nil, nil, err
 		}
 		return &spanner.ResultSetStats{QueryPlan: &qp}, nil, nil
 	} else if _, ok := jsonObj["stats"]; ok {
 		var rs spanner.ResultSet
 		err = protoyaml.Unmarshal(b, &rs)
 		if err != nil {
-			return nil, nil,  err
+			return nil, nil, err
 		}
 		return rs.GetStats(), rs.GetMetadata().GetRowType(), nil
 	}
